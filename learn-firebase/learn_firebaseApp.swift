9b77b286c2e4fe39b854e165a4810779ab13a1c5
Magistra <magistraagis@gmail.com>
2023-11-19 23:20:56 +0800

    Initial Commit

---
+++ learn-firebase/learn_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_firebaseApp.swift
+//  learn-firebase
+//
+//  Created by magistra aptam on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct learn_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
