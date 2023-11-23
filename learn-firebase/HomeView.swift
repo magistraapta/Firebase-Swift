30f015fe8c80e2cdd5e987239003d5ca931be8df
Magistra <magistraagis@gmail.com>
2023-11-23 13:54:33 +0800

    add new view

---
+++ learn-firebase/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  learn-firebase
+//
+//  Created by magistra aptam on 22/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
