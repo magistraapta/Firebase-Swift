71433a2982fdd6238f9fce9599751ca4484aa5f3
Magistra <magistraagis@gmail.com>
2023-11-23 13:55:13 +0800

    add more functions for user settings

--- learn-firebase/Authentication/SettingsView.swift
+++ learn-firebase/Authentication/SettingsView.swift
@@ -11,6 +11,28 @@ final class SettingsViewModel: ObservableObject {
     func logOut() throws{
         try AuthenticationManager.shared.userSignOut()
     }
+    
+    func updateEmail() async throws {
+        let email = "mynew@email.com"
+        try await AuthenticationManager.shared.updateEmail(email: email)
+    }
+    
+    func updatePassword() async throws {
+        let password = "newPassword123"
+        try await AuthenticationManager.shared.updatePassword(password: password)
+    }
+    
+    func resetPassword()async throws{
+        let authUser = try AuthenticationManager.shared.getAuthUser()
+        
+        guard let email = authUser.email else {
+            throw URLError(.fileDoesNotExist)
+        }
+        try await AuthenticationManager.shared.resetPassword(email: email)
+    }
+    func userEmailVerification() async throws {
+        try await AuthenticationManager.shared.verificationEmail()
+    }
 }
 
 struct SettingsView: View {
@@ -30,6 +52,60 @@ struct SettingsView: View {
             } label: {
                 Text("Log Out")
             }
+            
+            Button {
+                Task{
+                    do{
+                        try await settingVM.updateEmail()
+                        print("email updated")
+                    }catch{
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Update Email")
+            }
+            
+            Button {
+                Task{
+                    do{
+                        try await settingVM.resetPassword()
+                        print("email reseted")
+                    }catch{
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Reset Password")
+            }
+            
+            Button {
+                Task{
+                    do{
+                        try await settingVM.userEmailVerification()
+                        print("verification send")
+                    }catch{
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Send Verification")
+            }
+            
+            Button {
+                Task{
+                    do{
+                        try await settingVM.updatePassword()
+                        print("password updated")
+                    }catch{
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Update password")
+            }
+
+
 
         }
         .navigationTitle("Settings")
